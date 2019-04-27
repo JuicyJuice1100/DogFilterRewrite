065ab5df6160fbbb72398da10eb473167caee6b2
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2019-04-27 18:54:49 -0500

    created a messages component, config file, and created some functions for services

---
+++ src/app/config/dogs.config.ts
@@ -0,0 +1,6 @@
+export const DogConfig = {
+  url: {
+    dogBreeds: 'https://dog.ceo/api/breeds/list',
+    dogBreedImages: 'https://dog.ceo/api/breed/'
+  }
+}
--- src/app/services/dogs/dogs.service.ts
+++ src/app/services/dogs/dogs.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { DogConfig } from '../../config/dogs.config';
+
+import { MessagesService } from '../messages/messages.service';
+
+import { DogBreed } from '../../classes/dogBreed';
+import { DogImage } from '../../classes/dogImage';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogsService {
 
-constructor() { }
+
+  constructor(private http: HttpClient, private messageService: MessagesService) { }
+
+  private log(message: string) {
+    this.messageService.add('DogsSerivce: ${message}');
+  }
+
+  public getDogBreeds(): Observable<Array<DogBreed>> {
+    return this.http.get<Array<DogBreed>>(DogConfig.url.dogBreeds)
+  }
 
 }
