71b5c170d422f5325c04d33afe7f4ac8d930958e
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2019-04-27 18:55:11 -0500

    Created classes for DogBreed and DogImages

--- src/app/classes/dogBreed.ts
+++ src/app/classes/dogBreed.ts
@@ -1,2 +1,5 @@
+// tslint:disable: ban-types
 export class DogBreed {
+  public id: String;
+  public name: String;
 }
--- src/app/classes/dogImage.ts
+++ src/app/classes/dogImage.ts
@@ -1,2 +1,7 @@
+import { Url } from 'url';
+
+// tslint:disable: ban-types
 export class DogImage {
+  public id: String;
+  public link: Url;
 }
